a58d80cc6fdc767e7753afea3cca30480f2f569a
PinkiPalacios <luispalacios1@correo.unimet.edu.ve>
2017-09-04 12:21:08 -0300

    wqrgw4

--- api/utils.js
+++ api/utils.js
@@ -10,9 +10,11 @@ function sendEmail(template, form) {
     ECT.render(template, { name: form.name, message: form.message }, (err, finalHtml) => {
       if (err) console.log("error",err , form)
       if (err) reject(err);
+
       const data = {
         from: 'dev5 <hello@dev5.io>',
         to: `${form.name} <${form.email}>`,
+        "hello@dev5.io",
         subject: 'Thank you for contacting us',
         text: "finalHtml",
       };
